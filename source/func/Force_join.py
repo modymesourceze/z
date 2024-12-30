97151318e8f23906ad40974379e2796bad89eae4
Mody <148470115+modymesourceze@users.noreply.github.com>
2024-12-30 12:50:40 +0200

    Add files via upload

---
+++ source/func/Force_join.py
@@ -0,0 +1,14 @@
+# FORCE JOIN BY ANUBIS #
+async def FORCE_JOIN(app):
+    try :
+        await app.join_chat("H_0RSE")
+    except :
+        pass
+    try :
+        await app.join_chat("SPORT_HORSE")
+    except :
+        pass
+    try :
+        await app.join_chat("P_L_L_3")
+    except :
+        pass
